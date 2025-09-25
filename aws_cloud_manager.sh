c80101fda734ed85ec801d3b906b7008744e0b40
olanrewaju salami <82536846+slarry07@users.noreply.github.com>
2025-09-25 21:41:55 +0100

    Create aws_cloud_manager.sh

---
+++ aws_cloud_manager.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Checking the number of arguments
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <environment>"
+    exit 1
+fi
+
+# Accessing the first argument
+ENVIRONMENT=$1
+
+# Acting based on the argument value
+if [ "$ENVIRONMENT" == "local" ]; then
+  echo "Running script for Local Environment..."
+elif [ "$ENVIRONMENT" == "testing" ]; then
+  echo "Running script for Testing Environment..."
+elif [ "$ENVIRONMENT" == "production" ]; then
+  echo "Running script for Production Environment..."
+else
+  echo "Invalid environment specified. Please use 'local', 'testing', or 'production'."
+  exit 2
+fi
